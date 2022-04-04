9253166297ac56fbbf6386929639e1e63ec7f662
Taykhoom Dalal <tdalal@g.ucla.edu>
2022-04-04 11:10:33 -0700

    added code to parse filename

--- Project1/server.c
+++ Project1/server.c
@@ -14,12 +14,29 @@
 #define BACKLOG 10 /* pending connections queue size */
 #define MESSAGE_SIZE 1024
 
+char* parse_request(char *request) {
+
+    char* start = strchr(request, '/') + 1; // find the first '/', and then skip the '/'
+
+    for(int i = 0; i < strlen(start); i++) {
+        if(start[i] == ' ') { // find the space after the file name
+            char* fname = malloc(i + 1);
+            strncpy(fname, start, i);
+            fname[i] = '\0'; // terminate the string
+            return fname; // return the file name
+        }
+    }
+
+    return NULL; // if no file name is found
+}
+
 int main(int argc, char *argv[])
 {
   int sockfd, new_fd; /* listen on sock_fd, new connection on new_fd */
   struct sockaddr_in server_addr; /* my address */
   struct sockaddr_in client_addr; /* connector addr */
   int sin_size;
+  char* filename;
 
   // Create a socket
   if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) == -1) //AF_INET and PF_INET are the same
@@ -62,16 +79,21 @@ int main(int argc, char *argv[])
     // read from the socket
     char buffer[MESSAGE_SIZE];
     int nbytes;
-    if ((nbytes = recv(new_fd, buffer, sizeof(buffer), 0)) == -1) {
+    if ((nbytes = recv(new_fd, buffer, MESSAGE_SIZE, 0)) == -1) { //recv is usually used to read from a socket whereas read is used to read from a file
       perror("recv");
       exit(-1);
     }
     
     printf("Server: received %d bytes\n", nbytes);
     printf("Server: received the following message\n%s", buffer);
+
+    filename = parse_request(buffer);
+    printf("Server: parsed filename: %s\n", filename);
+
     close(new_fd);
     break;
   }
   close(sockfd);
+  free(filename);
   return 0;
 }
