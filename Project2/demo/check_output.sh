5f1318b9d872ca23352db720f437ba9cae2f058f
Taykhoom Dalal <tdalal@g.ucla.edu>
2022-06-01 18:42:04 -0700

    after making all the files, run this script to check all at once

---
+++ Project2/demo/check_output.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Test 1:
+echo 'Test 1: 100B no loss client'
+python3 tests/test_no_loss.py output/100B_no_loss_client.txt
+
+# Test 2:
+echo 'Test 2: 100KB no loss client'
+python3 tests/test_no_loss.py output/100KB_no_loss_client.txt
+
+# Test 3:
+echo 'Test 3: 1MB no loss client'
+python3 tests/test_no_loss.py output/1MB_no_loss_client.txt
+
+# Test 4:
+echo 'Test 4: 100B loss client'
+python3 tests/test_loss_gbn.py output/100B_loss_client.txt
+
+# Test 5:
+echo 'Test 5: 100KB loss client'
+python3 tests/test_loss_gbn.py output/100KB_loss_client.txt
+
+# Test 6:
+echo 'Test 6: 1MB loss client'
+python3 tests/test_loss_gbn.py output/1MB_loss_client.txt
