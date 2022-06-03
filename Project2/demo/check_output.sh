369a4e3892c14e684c9c0c74ec5363349903a70a
Taykhoom Dalal <tdalal@g.ucla.edu>
2022-06-02 23:25:36 -0700

    added diff check to test

--- Project2/demo/check_output.sh
+++ Project2/demo/check_output.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 
 # Test 1:
-echo 'Test 1: 100B no loss client'
+printf 'Test 1: 100B no loss client\n'
 python3 tests/test_no_loss.py output/100B_no_loss_client.log
+diff input/100B.txt output/100B_no_loss_transfer.log
 
 # Test 2:
-echo 'Test 2: 100KB no loss client'
+printf '\nTest 2: 100KB no loss client\n'
 python3 tests/test_no_loss.py output/100KB_no_loss_client.log
+diff input/100KB.txt output/100KB_no_loss_transfer.log
 
 # Test 3:
-echo 'Test 3: 1MB no loss client'
+printf '\nTest 3: 1MB no loss client\n'
 python3 tests/test_no_loss.py output/1MB_no_loss_client.log
+diff input/1MB.bin output/1MB_no_loss_transfer.log
 
 # Test 4:
-echo 'Test 4: 100B loss client'
+printf '\nTest 4: 100B loss client\n'
 python3 tests/test_loss_gbn.py output/100B_loss_client.log
+diff input/100B.txt output/100B_loss_transfer.log
 
 # Test 5:
-echo 'Test 5: 100KB loss client'
+printf '\nTest 5: 100KB loss client\n'
 python3 tests/test_loss_gbn.py output/100KB_loss_client.log
+diff input/100KB.txt output/100KB_loss_transfer.log
 
 # Test 6:
-echo 'Test 6: 1MB loss client'
+printf '\nTest 6: 1MB loss client\n'
 python3 tests/test_loss_gbn.py output/1MB_loss_client.log
+diff input/1MB.bin output/1MB_loss_transfer.log
